9789db93b9566f94fd9c0b9af80132d17c7c328e
Lucas <correa.l@icloud.com>
2024-05-19 19:56:21 -0300

    Subindo Aulas

---
+++ NextJS/blog-strapi-app/src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from 'next/dist/shared/lib/router/router';
+import { GlobalStyles } from '../styles/global-styles';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />;
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default MyApp;
