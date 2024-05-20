e84867f10af770465814e97fc964569336ce774d
Lucas <correa.l@icloud.com>
2024-05-19 21:08:46 -0300

    Subindo Aulas

--- NextJS/blog-strapi-app/src/styles/global-styles.ts
+++ NextJS/blog-strapi-app/src/styles/global-styles.ts
@@ -1,7 +1,16 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
+
     body {
-        background: red;
+        font-family: "Inter", sans-serif;
+        background: cyan;
+        display: flex;
+        justify-content: center;
+    }
+
+    body h1 {
+        margin-top: 4rem;
     }
 `;
